0e11f5f23d8fcdbce45f62ffe88c2afd919cc61f
Brendan Coen <Brendan.Coen@kamaq.co.uk>
2025-03-26 17:37:38 +0000

    Upgrade test projects from .NET 8 to .NET 9

--- src/Kamaq.Finsights.Worker/obj/Debug/net9.0/Kamaq.Finsights.Worker.AssemblyInfo.cs
+++ src/Kamaq.Finsights.Worker/obj/Debug/net9.0/Kamaq.Finsights.Worker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kamaq.Finsights.Worker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bb79f4720b838af73e91f7071fea6c66dd94de0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kamaq.Finsights.Worker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kamaq.Finsights.Worker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- tests/Kamaq.Finsights.Application.Tests/obj/Debug/net9.0/Kamaq.Finsights.Application.Tests.AssemblyInfo.cs
+++ tests/Kamaq.Finsights.Application.Tests/obj/Debug/net9.0/Kamaq.Finsights.Application.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kamaq.Finsights.Application.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bb79f4720b838af73e91f7071fea6c66dd94de0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kamaq.Finsights.Application.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kamaq.Finsights.Application.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
